da31f39c006f2fb73626da472a038b9e3ac2eca8
IsaacTranquilo <mutungaisaac76@gmail.com>
2024-05-05 23:17:22 +0300

    commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''A script that reads stdin line by line and computes metrics'''
 
+
 import sys
 
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
@@ -11,11 +12,9 @@ counter = 0
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
-        print("Line list:", line_list)  # Add this line for debugging
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            print("Code:", code, "Size:", size)  # Add this line for debugging
             if code in cache.keys():
                 cache[code] += 1
             total_size += size
@@ -29,11 +28,10 @@ try:
                     print('{}: {}'.format(key, value))
 
 except Exception as err:
-    print("Error:", err)  # Add this line for debugging
+    pass
 
 finally:
     print('File size: {}'.format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-
